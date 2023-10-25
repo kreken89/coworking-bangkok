508086f92b5609964478f56d6c819892831ac78d
Mikael Krefors <krefors@hotmail.com>
2023-10-25 13:58:05 +0200

    Map added

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,9 @@ import Modal from "./Modal";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
+import { useForm, FieldValues } from 'react-hook-form';
+import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
 
 enum STEPS {
     CATEGORY = 0,
@@ -18,11 +21,49 @@ enum STEPS {
     PRICE = 5,
 }
 
-
 const RentModal = () => {
     const rentModal = useRentModal();
 
     const [step, setStep] = useState(STEPS.CATEGORY);
+
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        watch,
+        formState: {
+        errors,
+    },
+    reset
+} = useForm<FieldValues>({
+    defaultValues: {
+        category: "",
+        location: null,
+        guestCount: 1,
+        roomCount: 1,
+        bathroomCount: 1,
+        imageSrc: '',
+        price: 1,
+        title: '',
+        description: '',
+        }
+    });
+
+    const category = watch("category");
+    const location = watch('location');
+
+    const Map = useMemo(() => dynamic(() => import("../Map"), {
+      ssr: false,
+    }), [location]);
+
+
+    const setCustomValue = (id: string, value: any) => {
+        setValue(id, value, {
+            shouldValidate: true,
+            shouldDirty: true,
+            shouldTouch: true,
+        })
+    }
 
     const onBack = () => {
         setStep((value) => value - 1);
@@ -49,41 +90,58 @@ const RentModal = () => {
     }, [ step ]);
 
     let bodyContent = (
-        <div className="flex flex-col gap-8">
-            <Heading 
-                title="Which of these best describes your space?"
-                subtitle="Pick a category"
-            />
-            <div 
-                className="
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Which of these best describes your space?"
+          subtitle="Pick a category"
+        />
+        <div
+          className="
                     grid 
                     grid-cols-1 
                     md:grid-cols-2 
                     gap-3 
                     max-h-[50vh] 
                     overflow-y-auto
-                "
-            >
-                {categories.map((item) => (
-                  <div key={item.label} className="col-span-1">
-                    <CategoryInput 
-                        onClick ={() => {}}
-                        selected={false}
-                        label={item.label}
-                        icon={item.icon}
-                    />
-
-                  </div>  
-                ))}
+                ">
+          {categories.map((item) => (
+            <div key={item.label} className="col-span-1">
+              <CategoryInput
+                onClick={(category) => 
+                    setCustomValue('category', category)}
+                selected={category === item.label}
+                label={item.label}
+                icon={item.icon}
+              />
             </div>
+          ))}
         </div>
-    )
+      </div>
+    );
+
+    if (step === STEPS.LOCATION) {
+        bodyContent = (
+          <div className="flex flex-col gap-8">
+            <Heading
+              title="Where's your space located?"
+              subtitle="Help guests find your space"
+            />
+            <CountrySelect
+              value={location}
+              onChange={(value) => setCustomValue('location', value)}
+            />
+            <Map 
+                center={location?.latlng}
+            />
+          </div>
+        );
+    }
 
   return (
     <Modal 
         isOpen={rentModal.isOpen}
         onClose={rentModal.onClose}
-        onSubmit={rentModal.onClose}
+        onSubmit={onNext}
         actionLabel={actionLabel}
         secondaryActionLabel={secondaryActionLabel}
         secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
